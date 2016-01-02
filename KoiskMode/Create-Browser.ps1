9106afdec080e5ad0dbb8af658aaa0393d6c93d6
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2016-01-02 09:00:51 -0800

    trying out youtube URL API

---
+++ KoiskMode/Create-Browser.ps1
@@ -0,0 +1,12 @@
+﻿$chromePath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+$chromeArguments = '--new-window --incognito'
+# if Window not moved (especially on machine start) - try increaing the delay.
+$chromeStartDelay = 3
+
+Set-Location $PSScriptRoot
+. .\HelperFunctions.ps1
+
+# Kill all running instances
+# &taskkill /im chrome* /F
+
+Chrome-Kiosk 'http://www.bbc.com/' -MonitorNum 2
