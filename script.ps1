255509faf1eee04c01645b2661507e6a1d49bc13
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2016-01-02 22:58:55 -0800

    got all data from youtube

--- script.ps1
+++ script.ps1
@@ -1,11 +1,16 @@
+# Dumps all gallileo video to csv file
+# position, videoId, seen, published, title, description (trimmed),
+
+
+
+
+
 $key = "AIzaSyCrb1f-QjPwO9w-sB6qQTNdQ-vEdjMx7Ek"
 $playlist = "UUpNzWUlO6PVb_v7chefBnig"
-$page = $null
 
 $parameters = [ordered] @{
     part = "snippet"
     maxResults = 50
-    pageToken = $page
     playlistId = $playlist
     fields = "nextPageToken%2Citems%2Fsnippet%2Ftitle%2Citems%2Fsnippet%2FpublishedAt%2Citems%2Fsnippet%2Fdescription%2Citems%2Fsnippet%2Fposition%2Citems%2Fsnippet%2FresourceId%2FvideoId" # nextPageToken,items/snippet/title,items/snippet/publishedAt,items/snippet/description,items/snippet/position,items/snippet/resourceId/videoId
     key = $key
@@ -14,8 +19,27 @@ $parameters = [ordered] @{
 $parametersText = ($parameters.Keys | where{ $parameters.$psitem } | foreach{ "$psitem=$($parameters.$psitem)" }) -join "&"
 $query = "https://www.googleapis.com/youtube/v3/playlistItems?$parametersText"
 
+# about 8 seconds in total
+$results = @()
+$response = Invoke-RestMethod $query
+$results += $response
 
-Invoke-RestMethod $query
+while( $response.nextPageToken )
+{
+    Write-Progress "Got next page" $response.nextPageToken
+    $parameters.pageToken = $response.nextPageToken
+    $parametersText = ($parameters.Keys | where{ $parameters.$psitem } | foreach{ "$psitem=$($parameters.$psitem)" }) -join "&"
+    $query = "https://www.googleapis.com/youtube/v3/playlistItems?$parametersText"
+    $response = Invoke-RestMethod $query
+    $results += $response
+}
+
+
+
+
+
+
+
 $a = Invoke-RestMethod $query
 $a.items
 $a.items.snippet
