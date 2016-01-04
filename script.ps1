5da036c1c1fddd3b4f3e83acb16f96ff76be84b2
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2016-01-04 11:40:22 -0800

    preparing to open brower on a new monitor on click

--- script.ps1
+++ script.ps1
@@ -1,13 +1,14 @@
-Add-Type –AssemblyName WindowsBase
-Add-Type –AssemblyName PresentationCore
-Add-Type –AssemblyName PresentationFramework
+# Build basic form
+Add-Type -AssemblyName WindowsBase
+Add-Type -AssemblyName PresentationCore
+Add-Type -AssemblyName PresentationFramework
 
 [xml] $xaml = @'
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
         SizeToContent="WidthAndHeight"
         MinWidth="300"
-        Title="Видео">
+        Title="Video">
     <Window.Resources>
         <Style TargetType="{x:Type Control}" x:Key="defaultStyle">
             <Setter Property="FontSize" Value="25"/>
@@ -25,20 +26,46 @@ $form = [Windows.Markup.XamlReader]::Load( (New-Object Xml.XmlNodeReader $xaml) 
 $stack = $form.FindName("stack")
 $lblTimer = $form.FindName("lblTimer")
 
-$stack.Children.Add( (New-Object System.Windows.Controls.Button -Property @{ Content = "test me" }) )
-$stack.Children.Add( (New-Object System.Windows.Controls.Button -Property @{ Content = "Галилео" }) )
+# Manually bind with data
+$lists = foreach( $file in ls $PSScriptRoot\Lists\*.csv )
+{
+    [PsCustomObject] @{
+        Name = $file.BaseName
+        FilePath = $file.FullName
+        Content = Import-Csv $file.FullName
+    }
+}
+$GLOBAL:timerStart = $null
 
+foreach( $item in $lists )
+{
+    $button = New-Object System.Windows.Controls.Button -Property @{
+        Name = $item.Name
+        Content = $item.Name
+    }
+    $button.add_Click({
+        if( -not $GLOBAL:timerStart )
+        {
+            $GLOBAL:timerStart = [datetime]::Now
+        }
 
-$timerStart = Get-Date
+        Write-Host "hi!"
+        $item.Content | Get-Random
+        Write-Host "hi!" ($item.Content | Get-Random)
+    })
+    $stack.Children.Add( $button ) | Out-Null
+}
 
 $timer = New-Object System.Windows.Threading.DispatcherTimer
-$timer.Interval = [timeSpan]::FromSeconds(0.1)
+$timer.Interval = [timeSpan]::FromSeconds(1)
 $timer.add_Tick({
-    $elapsed = (Get-Date) - $timerStart
-    $lblTimer.Content = "{0:hh\:mm\:ss}" -f $elapsed
+    if( $GLOBAL:timerStart )
+    {
+        $elapsed = [datetime]::Now - $GLOBAL:timerStart
+        $lblTimer.Content = $elapsed.ToString("hh\:mm\:ss")
+    }
 })
 $timer.Start()
-
 
 $form.ShowDialog() | Out-Null
 
