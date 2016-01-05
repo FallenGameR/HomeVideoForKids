4d73b40ad00cd3fcc6a3ae81a846ebf7390247e6
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2016-01-04 22:12:18 -0800

    Correct window closing with debug output

--- script.ps1
+++ script.ps1
@@ -1,8 +1,38 @@
-# Build basic form
+# Dependencies
+Set-Location $PSScriptRoot
 Add-Type -AssemblyName WindowsBase
 Add-Type -AssemblyName PresentationCore
 Add-Type -AssemblyName PresentationFramework
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName Microsoft.VisualBasic
 
+Add-Type -Path Tomin.Tools.KioskMode.dll
+
+$WinAPI = [Tomin.Tools.KioskMode.WinApi]
+$Helpers = [Tomin.Tools.KioskMode.Helper]
+
+function Get-ChromeHandle( [switch] $wait )
+{
+    Write-Host "peaking"
+    $window = Get-Process -Name chrome -ea Ignore | where MainWindowTitle
+
+    if( $wait )
+    {
+        while( -not $window )
+        {
+            Start-Sleep -Seconds 0.1
+            $window = Get-Process -Name chrome -ea Ignore | where MainWindowTitle
+            Write-Host "searching"
+        }
+    }
+
+    if( $window )
+    {
+        $window.MainWindowHandle
+    }
+}
+
+# Build basic form
 [xml] $xaml = @'
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
@@ -27,7 +57,7 @@ $form = [Windows.Markup.XamlReader]::Load( (New-Object Xml.XmlNodeReader $xaml) 
 $stack = $form.FindName("stack")
 $lblTimer = $form.FindName("lblTimer")
 
-# Manually bind with data
+# Reading data
 $lists = foreach( $file in ls $PSScriptRoot\Lists\*.csv )
 {
     [PsCustomObject] @{
@@ -36,8 +66,21 @@ $lists = foreach( $file in ls $PSScriptRoot\Lists\*.csv )
         Content = Import-Csv $file.FullName
     }
 }
-$GLOBAL:timerStart = $null
 
+# Setting up timer
+$GLOBAL:timerStart = $null
+$timer = New-Object System.Windows.Threading.DispatcherTimer
+$timer.Interval = [timeSpan]::FromSeconds(1)
+$timer.add_Tick({
+    if( $GLOBAL:timerStart )
+    {
+        $elapsed = [datetime]::Now - $GLOBAL:timerStart
+        $lblTimer.Content = $elapsed.ToString("hh\:mm\:ss")
+    }
+})
+$timer.Start()
+
+# Binding data
 foreach( $item in $lists )
 {
     $button = New-Object System.Windows.Controls.Button -Property @{
@@ -51,37 +94,47 @@ foreach( $item in $lists )
             $GLOBAL:timerStart = [datetime]::Now
         }
 
-        # Cleaning up
-        #Get-Process chrome -ea Ignore | kill
-
-        # Prepare Chrome
-        $chromePath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-        $chromeArguments = '--new-window'
-        $chromeStartDelay = 3 # if window not moved (especially on machine start) - try increaing the delay
-        Set-Location $PSScriptRoot
-        . .\HelperFunctions.ps1
-
-        # Get URL to show
+        # Getting video to show
         $video = $item.Content | Get-Random
         $url = "https://www.youtube.com/embed/$($video.VideoId)?autoplay=1"
+        $txtDescription.Text = $video.title + "`r`n" + $video.description
+
+        # Cleaning up
+        Get-ChromeHandle | foreach {
+            Write-Host "killing $psitem"
+            $Helpers::SendKey($psitem, '%{F4}')
+            while( Get-Process chrome -ea Ignore )
+            {
+                Write-Host "waiting for kill $psitem"
+                Start-Sleep -Seconds 0.1
+            }
+        }
 
         # Show video on second monitor
-        Chrome-Kiosk $url -MonitorNum 2
+        Write-Host "starting"
+        Start-Process 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' "--new-window $Url"
+        Get-ChromeHandle -Wait | foreach {
+            Write-Host "moving $psitem"
+            $WinAPI::ShowWindow($psitem, [Tomin.Tools.KioskMode.Enums.ShowWindowCommands]::Restore)
+            $Helpers::MoveToMonitor($psitem, 2)
+            Write-Host "enlarging $psitem"
+            $Helpers::SendKey($psitem, '{F11}')
+        }
     })
     $stack.Children.Add( $button ) | Out-Null
 }
 
-$timer = New-Object System.Windows.Threading.DispatcherTimer
-$timer.Interval = [timeSpan]::FromSeconds(1)
-$timer.add_Tick({
-    if( $GLOBAL:timerStart )
-    {
-        $elapsed = [datetime]::Now - $GLOBAL:timerStart
-        $lblTimer.Content = $elapsed.ToString("hh\:mm\:ss")
-    }
-})
-$timer.Start()
+$txtDescription = New-Object System.Windows.Controls.TextBlock -Property @{
+    Name = "txtDescription"
+    FontSize = "16"
+    MaxWidth = "300"
+    TextWrapping = "Wrap"
+    Margin = "10"
+    Visibility = "Collapsed"
+}
+$stack.Children.Add( $txtDescription ) | Out-Null
 
+# Showing form to the user
 $form.ShowDialog() | Out-Null
 
 
